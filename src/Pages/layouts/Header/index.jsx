0d0cc45976060754b3ba2435d9eb9544c2fb473b
YKIM <dyounggkim@gmail.com>
2022-11-30 11:53:38 +0000

    changed img src

--- src/Pages/layouts/Header/index.jsx
+++ src/Pages/layouts/Header/index.jsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { artsActions } from "../../../store/store";
 import LoadingSpinner from "../../LoadingSpinner";
 import SearchForm from "../../Forms";
+import github_logo from '../../../../assets/github_logo.png'
 
 const Header = () => {
     const { name } = useParams();
@@ -30,14 +31,13 @@ const Header = () => {
 
   let content = searchArray.length === 0 && textInput && !isLoading ? <h1>no repos for this user</h1> :  <Outlet /> 
 
-  
 
     return( 
         <>
           <nav id='nav'>  
         <SearchForm />
            </nav> 
-        {!textInput && !isLoading && <img src='../../assets/github_logo.png' /> }
+        {!textInput && !isLoading && <img src='../../../../assets/github_logo.png' /> }
         {!isError && content}
         {isError && displayError}
         {isLoading && textInput && <LoadingSpinner />}
